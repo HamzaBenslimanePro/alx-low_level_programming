a64faf39851bbdc29c1f16474cdeb98e0f02abfc
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-05 12:58:21 -0700

    just another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line."
- * Return 0.
+ * main - prints "Programming is like building a multilingual puzzle and then new line"
  */
-int main(void)
-{
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return 0;
 }
