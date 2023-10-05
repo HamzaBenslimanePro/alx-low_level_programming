aca44197248a171dd91f3b82dfa5ffa5beab4cb8
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-05 13:19:00 -0700

    just another commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description:"Write a C program that prints exactly etc ..."
+ * Return always 0 (always)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
