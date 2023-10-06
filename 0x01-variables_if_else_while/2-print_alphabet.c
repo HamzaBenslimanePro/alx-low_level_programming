0ba136fdfd5d2b6c3359176ee26453610da72c66
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 11:58:13 -0700

    just another commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * Description: A C program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
