3f5e709a6c307fd3d86339362084b090723dcbcb
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 11:08:34 -0700

    just another commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry 
- * description : randomize a number and tell its sign
- * return: Always 0 (success)
+ * main - Entry
+ * Description: randomize a number and tell its sign
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,11 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-		printf("%d is positive", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero", n);
-	else if (n<0)
-		printf("%d is negative", n);
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
