f9437b6e5c7ed942d36992ab06f5b8fe087b352d
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 14:51:29 -0700

    just another commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,26 @@
 
 int main(void)
 {
-	int d1, d2;
-	int sep1 = 0;
-	int sep2 = 0;
+	int d1 = 0, d2;
 
-	d1 = 48;
-	while (d1 < 58)
+	while (d1 <=  9)
 	{
-		d2 = d1 + 1;
-	while (d2 < 58)
-	{
-		putchar(sep1);
-		putchar(sep2);
-		putchar(d1);
-		putchar(d2);
-		d2++;
-		sep1 = ',';
-		sep2 = ' ';
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1 + 48);
+				putchar(d2 + 48);
+				if (d1 + d2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
 	}
-	d1++;
-}
 	return (0);
 }
