58f6818f3858e6bbdd7412bf8dffebed4f89fc1d
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 15:33:13 -0700

    just another commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			if (d2 != d1)
 			{
-				putchar((d1 / 10) +48);
-				putchar((d1 % 10) +48);
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
 				putchar(' ');
-				putchar((d2 / 10) +48);
-				putchar((d2 % 10) +48);
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
 
 				if (d1 != 98 || d2 != 99)
 				{
