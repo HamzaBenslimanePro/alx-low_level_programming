ea2dd0ff15b542b4bd1029c82bf785e9bf416483
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 15:14:13 -0700

    just another commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,33 +8,28 @@
 
 int main(void)
 {
-	int d1 = 0, d2, d3;
+        int a, b, c;
 
-	while (d1 <=  9)
-	{
-		d2 = 0;
-		while (d2 <= 9)
-		{
-			d3 = 0;
-			while (d2 <= 9)
-			{
-				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3)
-				{
-					putchar(d1 + 48);
-					putchar(d2 + 48);
-					putchar(d3 + 48);
-					if (d1 + d2 + d3 != 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				d3++;
-			}
-			d2++;
-		}
-		d1++;
-	}
-	putchar('\n');
-	return (0);
-}
+        for (a = 0; a < 8; a++)
+        {
+                for (b = a + 1; b < 9; b++)
+                {
+                        for (c = b + 1; c < 10; c++)
+                        {
+                                if (a != b && a != c && b != c)
+                                {
+                                        putchar(a + '0');
+                                        putchar(b + '0');
+                                        putchar(c + '0');
+                                        if (a + b + c < 24)
+                                        {
+                                                putchar(',');
+                                                putchar(' ');
+                                        }
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
+}}
