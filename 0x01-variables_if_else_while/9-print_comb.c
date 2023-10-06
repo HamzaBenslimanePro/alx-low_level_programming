57414929101a94cbd8ca0614169569dab357f5cd
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 13:19:02 -0700

    just another commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * Description: A C program that prints a list of numbers 0-9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch < '9'; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
