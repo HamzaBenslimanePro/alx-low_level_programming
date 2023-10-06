3229093954e832df30151ef75db7cdc221f8e0c7
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-06 13:47:29 -0700

    just another commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,17 @@
  * Description: A C program that prints a list of numbers 0-9
  * Return: Always 0 (Success)
  */
-int main() {
-    int d1, d2;
-    const char *sep = "";
+int main()
+{
+	char d1;
+	const char *sep = "";
 
-    for (d1 = '0'; d1 <= '9'; d1++) {
-            fputs(sep, stdout);
-            sep = ", ";
-            putchar(d1);
-    }
-    putchar('\n');
-    return 0;
+	for (d1 = '0'; d1 <= '9'; d1++) 
+{
+		fputs(sep, stdout);
+		sep = ", ";
+		putchar(d1);
+}
+	putchar('\n');
+	return 0;
 }
