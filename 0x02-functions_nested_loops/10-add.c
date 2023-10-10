c9d9ceb30b899736f98a7772511ece5d979c960c
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-10 15:38:49 -0700

    just another commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,16 @@
 
 /**
  * add - blabla
- *
+ * @a: int to add to b
+ * @b: int to add to a
+ * Return: sum of a and b
  */
 
-int add()
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
