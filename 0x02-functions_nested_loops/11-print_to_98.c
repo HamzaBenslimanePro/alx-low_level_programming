c12cb5358dc9cb96692cc7a810c898731d9b85bb
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-10 15:58:50 -0700

    just another commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,17 @@
 #include "main.h"
 /**
  * print_to_98 - blabla
- *
+ * @n: input
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int c;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
-		}
-	}
-	else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
-	}
+	if (n > 98)
+		for (c = n; c > 98; count--)
+			printf("%d, ", c);
+	else
+		for (c = n; c < 98; count++)
+			printf("%d, ", c);
+	printf("98\n");
 }
