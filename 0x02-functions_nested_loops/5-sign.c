b95c3b9975b34cebbdfc831ff686ce5b7b61e719
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-10 14:48:47 -0700

    just another commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - blabla
+ * @n: int to check
+ * Return: 1 and prints + if sup then 0
+ * prints 0 if eauql to zero
+ * -1 if less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
