7c2047f4e5691d7c723351e4cfc911cd47bd40cc
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-10 14:37:10 -0700

    just another commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * Return: 1 if letter, otherwise 0.
  */
 
-int _isalpha(int c)
+int _isalpha(int c);
 {
-	return ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
