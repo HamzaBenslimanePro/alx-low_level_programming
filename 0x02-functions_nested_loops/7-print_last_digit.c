c0a20002bdf3ddb981d0c3cf3b8c1b432a2b1b1a
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-10 15:13:26 -0700

    just another commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - blabla
+ * @n: the int to we worked on
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	a = n%10;
+
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+
+	return (a);
+}
