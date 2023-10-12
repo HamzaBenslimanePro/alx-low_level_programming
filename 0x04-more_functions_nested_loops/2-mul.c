2da50a3006a6d332558b34ff4f3ee3866b8892ea
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-12 13:55:26 -0700

    just another commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - blabla
+ * @a: int to multiply by b
+ * @b: int to multiply by a
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
