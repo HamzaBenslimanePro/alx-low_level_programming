a6d3ed81a8b092745214168488ff72ec718e6360
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-12 14:08:12 -0700

    just another commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,16 +7,11 @@ void print_numbers(void)
 	char ch;
 	int i;
 
-	i = 0;
-	while (i < 10)
+	ch = '0';
+	while (ch <= '9')
 	{
-		ch = '0';
-		while (ch <= '9')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++;
+		_putchar(ch);
+		ch++;
 	}
+	_putchar('\n');
 }
