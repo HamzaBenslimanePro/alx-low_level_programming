ffbcfb7d65297ef5cf9cd3bd62ad93b30ab70726
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-12 13:47:53 -0700

    just another commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - blabla
+ * @c: the char to be checked
+ * Return: 1 if letter, otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= '0' && c <= '9');
+}
