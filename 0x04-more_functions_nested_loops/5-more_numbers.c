bce2d795428ae3060c6aa4af171870cc27489b6d
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-12 14:56:39 -0700

    just another commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - blabla
+ * Description: A C program that prints a list of numbers 0-9
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 1; b <= 14 ; b++)
+		{
+			if (b >=10)
+				_putchar('1');
+			_putchar(b % 10 + '0');
+		}
+		_putchar ('\n');
+	}
+}
