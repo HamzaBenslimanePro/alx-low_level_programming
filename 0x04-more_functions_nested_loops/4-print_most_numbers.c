12c5c872605d611c9cd51e84ade10c802f92bebe
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-12 14:26:08 -0700

    just another commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,12 @@
  */
 void print_most_numbers(void)
 {
-	char ch = '0';
+	char ch ;
 
 	for (ch = '0'; ch <= '9'; ch++)
 	{
-		if (ch == '2' || ch == '4')
-			ch++;
-		_putchar(ch);
+		if (!(ch == '2' || ch == '4'))
+			_putchar(ch);
 	}
 	_putchar('\n');
 	return (0);
