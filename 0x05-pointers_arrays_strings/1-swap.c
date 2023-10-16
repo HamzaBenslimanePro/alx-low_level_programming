0ee763357cf4ab50fb7debfbb8a0b0cf2c240637
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-16 10:12:25 -0700

    just another commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - blabla
+ * @a: int to be switched
+ * @b: same direction here too
+ */
+
+void swap_int(int *a, int *b)
+{
+	int a = *a;
+	*a = *b;
+	*b = a;
+}
