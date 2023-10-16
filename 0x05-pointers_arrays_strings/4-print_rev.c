27fb3e50b2f7db2b03d6a00a1f65bec6968124a6
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-16 10:46:04 -0700

    just another commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - blabla
+ * @s: string used
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
