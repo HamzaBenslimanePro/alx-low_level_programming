4b4f155d607f864440e874404558450a92add0e6
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-16 12:42:04 -0700

    just another commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,9 @@
 void puts_half(char *str)
 {
 	int i, c, n;
-    
+
 	c = 0;
-    
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		c++;
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	n = c / 2;
 	if ((c % 2) == 1)
 	{
-		n =((c + 1) / 2);
+		n = ((c + 1) / 2);
 	}
 	for (i = n; str[i] != '\0'; i++)
 	{
