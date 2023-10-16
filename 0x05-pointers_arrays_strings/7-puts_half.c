9e20e0a27b3b4b4822e8e90e1489325b45b2838b
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-16 12:36:29 -0700

    just another commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - blabla
+ * @str: string used
+ */
+
+void puts_half(char *str)
+{
+    int i, c, n;
+    
+    c = 0;
+    
+    for (i = 0; str[i] != '\0'; str++)
+    {
+        c++;
+    }
+    n = c / 2;
+    if ((c % 2) == 1)
+    {
+        n =((c + 1) / 2);
+    }
+    for (i = n; str[i] != '\0'; i++)
+    {
+        putchar(str[i]);
+    }
+
+}
