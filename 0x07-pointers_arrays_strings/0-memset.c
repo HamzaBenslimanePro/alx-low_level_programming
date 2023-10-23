6b06d676d5f919d19291ffeaaffdd171add1ddab
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-23 12:21:08 -0700

    just another commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - blabla
+ * @n: firt byte to change
+ * @b: what it is changed into
+ * @s: the memory area pointed
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
