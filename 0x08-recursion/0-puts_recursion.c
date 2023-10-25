7e86b40f9387fa72c25ea72d403f44ad0870302c
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-25 12:21:31 -0700

    just another commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - blabla
+ * @a: string to be put
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(*s + 1);
+	}
+	else
+		_putchar('\n');
+
+}
