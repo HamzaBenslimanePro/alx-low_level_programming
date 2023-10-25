f48e2177242e941157bf9ba773aa0d2eb1aeebae
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-25 14:21:59 -0700

    just another commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,7 +26,7 @@ int is_prime_number(int n)
 int actual_prime_func(int n, int i)
 {
 	if (i == 1)
-		return (0);
+		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
 	return (actual_prime_func(n, i - 1));
