5b377d1f2ebc30daca5ccc3d2c8cccb2b7ca0d8b
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-27 12:52:43 -0700

    just another commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - blabla
+ * @argc: blabla
+ * @argv: blabla
+ * Return: return 0 success always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
