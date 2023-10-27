9e3582a34987fb1c629b9d10df1a0c6febd3ea69
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-27 13:06:10 -0700

    just another commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - blabla
+ * @argv: blabla
+ * @argc: blabla
+ * Return: Blabla
+ */
+
+int main(int argc, char *argv[])
+{
+	int M;
+
+	M = argv[0] * argv[1];
+	printf("%d\n", M);
+
+	return (0);
+}
