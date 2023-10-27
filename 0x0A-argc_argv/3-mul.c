c1a331fe5652e3756a340748bd62a84970e144fa
HamzaBenslimane <hamzasamiben@gmail.com>
2023-10-27 13:22:51 -0700

    just another commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-	
+
 	i = 0;
 	d = 0;
 	n = 0;
@@ -41,55 +41,29 @@ int _atoi(char *s)
 	}
 	if (f == 0)
 		return (0);
-		
 	return (n);
 }
 
 /**
-		
  * main - multiplies two numbers
-		
  * @argc: number of arguments
-		
  * @argv: array of arguments
-		
- *
-		
  * Return: 0 (Success), 1 (Error)
-		
  */
-		
 int main(int argc, char *argv[])
-		
 {
-		
 	int result, num1, num2;
-		
 
-		
 	if (argc < 3 || argc > 3)
-		
 	{
-		
 		printf("Error\n");
-		
 		return (1);
-		
 	}
-		
-
-		
 	num1 = _atoi(argv[1]);
-		
 	num2 = _atoi(argv[2]);
-		
 	result = num1 * num2;
-		
 
-		
 	printf("%d\n", result);
-		
 
-		
 	return (0);
 }
