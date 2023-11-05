4faf1f8a7d34934a93e7f4033a06965af2bfa6d4
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-05 04:52:43 -0800

    just another commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,23 +1,3 @@
-//#include "dog.h"
-//#include <stdio.h>
-//#include <stdlib.h>
-
-/**
- * print_dog - xxxxxxxxxxxxxxxxxxxx
- * @d: xxxxxxxxxxxxxxxxxxxxxx
- * Return: void
- */
-/*
-void print_dog(struct dog *d)
-{
-	if (d)
-	{
-		printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
-		printf("Age: %f\n", (*d).age);
-		printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
-	}
-}*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
