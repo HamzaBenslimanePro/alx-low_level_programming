26a377e3a6a25dabe3d3af15fd24d59d8b341770
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-05 03:34:02 -0800

    just another commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - blabla
+ * @name: xxxxxxxxxxxxxx
+ * @age: xxxxxxxxxxxxxxxxxxx
+ * @owner: xxxxxxxxxxxxxxxxxxxx
+ * @d: xxxxxxxxxxxxxxxxxxxxx
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	char *name;
+	float age;
+	char *owner;
+}
