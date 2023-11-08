7fff2cba15402b98e6f6921a9643c04f2f397ba5
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-08 07:26:12 -0800

    just another commit

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef func_point_h
+#define func_point_h
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar.c(char c);
+
+#endif
