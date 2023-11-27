72ee6f478178160ba496ea87edd463d688812a87
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-27 09:36:37 -0800

    just another commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * read_textfile - blabla
+ * @filename: blabla
+ * @letters: blabla
+ * Return: the number of letters in the file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t c;
+	int fd;
+	char buf[READ_BUF_SIZE * 8]
+
+
+	return (c);
+}
