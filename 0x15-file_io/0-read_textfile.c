92e06c3ec915c47a56b4e41af016a03c35b170f9
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-27 09:45:00 -0800

    just another commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * read_textfile - blabla
+/**
+ * read_textfile - reads and print text of a file
  * @filename: blabla
  * @letters: blabla
  * Return: the number of letters in the file
@@ -11,8 +11,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t c;
 	int fd;
-	char buf[READ_BUF_SIZE * 8]
+	char buf[READ_BUF_SIZE * 8];
 
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+	c = read(fd, &buf[0], letters);
+	c = write(STDOUT_FILENO, &buf[0], c);
+	close(fd);
 
 	return (c);
 }
