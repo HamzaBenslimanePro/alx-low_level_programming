59419585812aebf8c3078721c01b3c9783a66220
HamzaBenslimane <hamzasamiben@gmail.com>
2023-11-27 10:02:23 -0800

    just another commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,17 +12,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+        ssize_t lenr, lenw;
+        char *buffer;
 
-	if (!filename || !letters)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
+        if (filename == NULL)
+                return (0);
+        fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
-	return (bytes);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close (fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
